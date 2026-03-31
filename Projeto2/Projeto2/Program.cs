6b47dcfbdb4faa3da2cf3906007e8399e99f364f
AntonioBittencourt <antoniopedrocbittencourt@gmail.com>
2026-03-30 22:24:36 -0300

    Update Program.cs

--- Projeto2/Projeto2/Program.cs
+++ Projeto2/Projeto2/Program.cs
@@ -1,16 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Projeto2
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+         static void Main(string[] args)
         {
-            Controlador jogo = new Controlador("Antonio", "Pedro");
+
+
+            string nome1 = "";
+            string nome2 = "";
+            while (string.IsNullOrWhiteSpace(nome1))
+            {
+                Console.WriteLine("Coloque o nome do jogador 1:");
+                nome1 = Console.ReadLine();
+                Thread.Sleep(1000);
+            }
+            
+            while (string.IsNullOrWhiteSpace(nome2))
+            {
+                Console.WriteLine("Coloque o nome do jogador 2:");
+                nome2 = Console.ReadLine();
+                Thread.Sleep(1000);
+            }
+            Controlador jogo = new Controlador(nome1, nome2);
             jogo.Jogo();
 
 
